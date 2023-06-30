8626d23039849b6994d35b3781fdcd8f0af419d7
edgar <ematiguan908@gmail.com>
2023-06-30 17:24:58 -0400

    listar

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+nombre= 'Edgar'
+lista_nombres=['kao','chi','lan','lo']
 #ruta
 @app.route('/')
 #vista
-def index():
-    return render_template('base.html')
+def home():
+    return render_template('home.html', nombre=nombre, lista_nombres=lista_nombres)
+
+
