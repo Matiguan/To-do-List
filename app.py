737ae56f55421ba43d5c170ad63a892b1dbc6b25
edgar <ematiguan908@gmail.com>
2023-07-07 17:36:05 -0400

    enredo

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 
 app=Flask(__name__)
 
 nombre= 'Edgar'
-lista_nombres=['kao','chi','lan','lo']
+lista_tareas=[]
 #ruta
 @app.route('/')
 #vista
 def home():
-    return render_template('home.html', nombre=nombre, lista_nombres=lista_nombres)
+    return render_template('home.html', nombre=nombre, lista_tareas=lista_tareas)
 
+#ruta
+@app.route('/agregar', methods=['GET', 'POST'])
+#vista
+def agregar():
+    if request.method== 'POST':
+        nueva_tarea=request.form.get('tarea')
+        lista_tareas.append(nueva_tarea)
+    return redirect('/')
 
